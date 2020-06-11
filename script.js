7d8da2c7c5797582f4b6db3e698c7f60d32bf078
BewareShips <49000772+BewareShips@users.noreply.github.com>
2020-06-11 13:22:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let x = Math.floor(Math.random()* 10 + 1)
 
 document.querySelector('.guessSubmit').onclick = () =>{
   let y = document.querySelector('.guessField').value;
-  if(x===y){
+  if(x==y){
     alert("Congratulations you guessed it was "+ x );
   }else if(x<y){
     alert("Sorry try a smaller number"); 
