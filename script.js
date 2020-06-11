bfe6b69adf4693d90a0cb570aa44a68099950230
BewareShips <49000772+BewareShips@users.noreply.github.com>
2020-06-11 13:20:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let x = Math.floor(Math.random()* 10 + 1)
+
+document.querySelector('.guessSubmit').onclick = () =>{
+  let y = document.querySelector('.guessField').value;
+  if(x===y){
+    alert("Congratulations you guessed it was "+ x );
+  }else if(x<y){
+    alert("Sorry try a smaller number"); 
+  }else if(x>y){
+    alert("Sorry try a greater number")
+  }else{
+    alert("Please write a number ")
+  }
+}
